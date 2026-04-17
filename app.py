3fdc6da09d72b43727d07d22c253336f4b58b804
Govind Jadapalli <jadapalli.govind@tudip.com>
2026-04-17 18:25:26 +0530

    changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline ! \n New Change Made by ~Govind J !"
+    return "Hello from CI/CD Pipeline ! \n New Change Made by ~Govind Jadapalli !"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
